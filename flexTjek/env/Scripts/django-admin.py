15799856ebdf12e4f4dbe795d57302250164e07c
markus simonsen <markus.k.simonsen@gmail.com>
2022-06-27 13:06:24 +0200

    Add project files.

---
+++ flexTjek/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Markus Simonsen\Desktop\UNI\Robtek\2. sem\C++\flexTjek\flexTjek\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
